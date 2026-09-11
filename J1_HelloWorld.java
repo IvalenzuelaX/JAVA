e951262b8cb12d06d0c747c817e7be471de60b43
Alexander Valenzuela <ivalenzuelavaz@gmail.com>
2026-09-11 07:25:46 -0500

    Add files via upload

---
+++ J1_HelloWorld.java
@@ -0,0 +1,20 @@
+public class J1_HelloWorld {
+    public static void main(String[] args){
+        System.out.println("Hola Mundo!\nSoy Alexander");
+        
+        //Ejercicios
+        // 1) 
+        System.out.println("Alexander");
+        // 2) 
+        System.out.println("Hola\nMundo");
+        // 5) 
+        System.out.println("Tengo 20, mi color fav es el azul y vivo en Lima");
+        // 6)
+        System.err.println(); //muestra mensajes de error
+        System.out.println((char)52); //imprime el valor 52 del codigo ASCIII
+        System.out.println((int)'a'); //imprime el numero del codigo ASCIII que da 'a' */
+        // 7)
+        System.out.println("Hola");
+        System.out.println("Mundo");
+    }
+}
